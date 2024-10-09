9810b873a6531c6f0ef112585aa5f44f6966fcab
Felix <felix.wedler@t-online.de>
2024-10-10 01:44:52 +0200

    Included an empty FileLoader class

---
+++ include/storage/FileLoader.hpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#ifndef FILELOADER_HPP
+#define FILELOADER_HPP
+
+#endif //FILELOADER_HPP
+
+namespace storage {
+
+  class FileLoader {
+    private:
+    public:
+      FileLoader() {
+        std::cout << "FileLoader::FileLoader()" << std::endl;
+      }
+  };
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <storage/FileLoader.hpp>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    storage::FileLoader loader{};
     return 0;
 }
