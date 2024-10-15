e356fcf6fd619c20cb56ddf2fb71ab6af5a658a5
Felix <felix.wedler@t-online.de>
2024-10-15 02:25:16 +0200

    Continued work on the file reader

--- include/storage/FileLoader.hpp
+++ include/storage/FileLoader.hpp
@@ -1,18 +1,35 @@
-#include <iostream>
 
 #ifndef FILELOADER_HPP
 #define FILELOADER_HPP
+#include <string>
 
 #endif //FILELOADER_HPP
+
+#include <fstream>
+#include <vector>
 
 namespace storage {
 
   class FileLoader {
     private:
-    public:
-      FileLoader() {
-        std::cout << "FileLoader::FileLoader()" << std::endl;
+    const std::string& path;
+    constexpr std::ios_base::openmode mode = std::ios_base::in | std::ios_base::out;
+    std::fstream data;
+    std::vector<std::string> lines;
+
+    void readLines() {
+      //TODO: Check for error codes
+      while (!data.eof) {
+        std::getline(data, lines.back(), '\n');
       }
+    };
+
+    public:
+      FileLoader() = delete;
+      explicit FileLoader(const std::string& path) : path(path) {
+        data.open(path, mode);
+        readLines();
+      };
   };
 
 }
