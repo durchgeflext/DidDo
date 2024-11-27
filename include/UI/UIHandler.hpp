82bb07a629f4767d1977016fc6ba6ff50d976425
Felix <felix.wedler@t-online.de>
2024-11-27 01:57:16 +0100

    Implemented necessary changes to the FileLoader. This class is now only responsible for loading files, independently of the type of file. Added a function to print Infos to the Terminal

--- include/UI/UIHandler.hpp
+++ include/UI/UIHandler.hpp
@@ -6,14 +6,30 @@
 #include <string>
 
 namespace diddo::ui {
+  /**
+  * This class handles the user interface.
+  */
   class UIHandler {
     private:
 
     public:
-      static void warning(const std::string_view& origin, const std::string& message) {
-        std::cout << "WARNING:" << "[" << origin << "]" << " " << message << std::endl;
+      /**
+      * Prints an info message to the console.
+      */
+      static void info(const std::string_view& origin, const std::string& message) {
+        std::cout << "INFO: " << "[" << origin << "]" << " " << message << std::endl;
       }
 
+      /**
+      * Prints a warning message to the console.
+      */
+      static void warning(const std::string_view& origin, const std::string& message) {
+        std::cout << "WARNING: " << "[" << origin << "]" << " " << message << std::endl;
+      }
+
+      /**
+      * Prints an error message to the console.
+      */
       static void error(const std::string_view& origin, const std::string& errorMessage) {
         std::cerr << "[" << origin << "]" << " " << errorMessage << std::endl;
       }
--- include/storage/FileLoader.hpp
+++ include/storage/FileLoader.hpp
@@ -1,52 +1,93 @@
 #pragma once
 
-#include <string>
+#include <filesystem>
 #include <fstream>
-#include <vector>
+#include <string>
 
 #include <UI/UIHandler.hpp>
 
 
 namespace diddo::storage {
 
-  ///This class handles loading files, as well as deleting and saving them
+  /**
+   *This class handles loading files, as well as deleting and saving them.
+   */
   class FileLoader {
     static constexpr std::string_view className = "FileLoader";
     static constexpr std::ios_base::openmode mode = std::ios_base::in | std::ios_base::out;
     const std::string& path;
-    std::fstream data;
-    std::vector<std::string> lines;
-
-    void readLines() {
-      while (!data.eof()) {
-        std::getline(data, lines.back(), '\n');
-        if (data.rdstate() == std::ios_base::badbit) {
-          ui::UIHandler::error(className.data(), "irrecoverable stream error when reading \"" + path + "\".");
-        }
-        if (data.rdstate() == std::ios_base::failbit) {}
-          ui::UIHandler::error(
-            className.data(), "reading \"" + path + "\" did not work, formatting or extraction error.");
-      }
-    };
+    std::shared_ptr<std::fstream> data;
 
     public:
-      FileLoader() = delete;
-      explicit FileLoader(const std::string& path) : path(path) {
-        data.open(path, mode);
-        readLines();
-      };
+    FileLoader() = delete;
 
-      ~FileLoader() {
-        if(!data.is_open()) {
-          ui::UIHandler::error(className,  "The file " + path + " has already been closed");
-          return;
-        }
-        data.close();
+    /**
+    * Constructor for the FileLoader class.
+    *
+    * @param path: Reference to the path, where the file is located
+    */
+    explicit FileLoader(const std::string& path) : path(path) {
+      data->open(path, mode);
+      ui::UIHandler::info(className, "The file " + path + " has been opened");
+    }
+
+    /**
+    * Destructor for the FileLoader class. Closes and saves the file.
+    */
+    ~FileLoader() {
+      if(!data->is_open()) {
+        ui::UIHandler::warning(className,  "The file " + path + " has already been closed");
+        return;
+      }
+      data->close();
+      if (data->rdstate() == std::fstream::failbit) {
+        ui::UIHandler::error(className, "The file " + path + " could not be closed");
+      }
+    }
+
+    /**
+    * Returns the contents of the file.
+    *
+    * @return: shared pointer to the file stream, which is both readable and writable
+    */
+    [[nodiscard("Did you want to ignore all the data?")]] std::shared_ptr<std::fstream> getContent() const {
+      if(!data->is_open()) {
+        ui::UIHandler::error(className, "The file " + path + " is not open");
+      }
+      return data;
+    }
+
+    /**
+    * Closes and deletes the file.
+    */
+    void deleteFile() {
+      data->close();
+      if (data->rdstate() == std::fstream::failbit) {
+        ui::UIHandler::warning(className, "The file " + path + " could not be closed correctly before deletion");
       }
 
-      std::unique_ptr<std::vector<std::string>> getLines() {
-        return std::make_unique<std::vector<std::string>>(lines);
+      std::error_code ec;
+      if (std::filesystem::remove(path, ec)) {
+        ui::UIHandler::info(className, "The file " + path + " has been deleted");
+        return;
       }
+
+      if (0 == ec.value()) {
+        ui::UIHandler::warning(className, "The file " + path + "could not be deleted, but didn't produce an error");
+        return;
+      }
+
+      if (ec.value() != 0) {
+        ui::UIHandler::error(className, "An error occurred while deleting the file " + path + ": " + ec.message());
+      }
+    }
+
+    void renameFile(std::string& newPath) {
+      //TODO: Implement renaming of files
+      ui::UIHandler::warning(className, "File renaming is not yet implemented");
+
+    }
+
   };
 
 }
